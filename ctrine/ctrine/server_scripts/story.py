33df2bc32fc208a0d6743f66ca511b254baa3cc0
Razzkutty <therazz007@gmail.com>
2024-03-21 17:31:28 +0530

    fix:fetching billing hours based on employee

--- ctrine/ctrine/server_scripts/story.py
+++ ctrine/ctrine/server_scripts/story.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 import frappe
 @frappe.whitelist()
-def fetch_hours(project_name,from_date):
+def fetch_hours(project_name,from_date,employee):
     total_hours=0
     from_date = datetime.strptime(from_date, '%Y-%m-%d').date()
     stories=frappe.get_all('Story',filters={'parent_project':project_name})
@@ -12,6 +12,6 @@ def fetch_hours(project_name,from_date):
             child_doc=story_doc.get('hours')
             for row in child_doc:
                 frappe.logger('row_date').exception(f"{type(row.date)},{type(from_date)},{row.date == from_date}")
-                if(row.date and row.date == from_date):
+                if(row.date and row.date == from_date and row.person== employee):
                     total_hours += int(row.hours)
         return total_hours
