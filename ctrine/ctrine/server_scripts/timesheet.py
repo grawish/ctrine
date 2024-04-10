782e558672c325fe4d3000f56bfcb1e8d21aa05a
Razzkutty <therazz007@gmail.com>
2024-04-10 15:48:12 +0530

    fix(Timesheet Doctype): 1.add precison for hours and billing hours fields in TimesheetDetails(ChildTable). 2.Remove the condition "Total hours for a same project should not exceed 24 hrs".

--- ctrine/ctrine/server_scripts/timesheet.py
+++ ctrine/ctrine/server_scripts/timesheet.py
@@ -9,28 +9,12 @@ def submit_overdue_timesheets():
     
     for timesheet in overdue_timesheets:
         try:
-            project_hours = {}
-            is_hours_valid = True
             # Get Timesheet document
             time_doc = frappe.get_doc("Timesheet", timesheet.get('name'))
             # Access child table entries
             timesheet_details = time_doc.get("time_logs")
-            # Calculate total hours for each project
-            for log in timesheet_details:
-                if log.project_name in project_hours:
-                    project_hours[log.project_name] += log.hours
-                else:
-                    project_hours[log.project_name] = log.hours
-            frappe.logger('ProjectHours').exception(project_hours)
-            # Check if any project exceeds 24 hours
-            for project, hours in project_hours.items():
-                if hours > 24:
-                    is_hours_valid = False
-                    break
-            
-            # Submit the timesheet if it's overdue and the hours are valid
-            if is_hours_valid:
-                start_date = timesheet.start_date
+            start_date = timesheet.start_date
+            if start_date:
                 days_to_sunday = (6 - start_date.weekday()) % 7  # Calculate days until Sunday
                 next_sunday = start_date + timedelta(days=days_to_sunday)
                 #frappe.logger('dayCheck').exception(f'ST:{start_date},End:{next_sunday}')
